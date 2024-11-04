925c4a9d6ca4417698efe7fd3ed984c69dafd1d4
Vitor Guimarães <vitorrawwr@gmail.com>
2024-11-04 22:09:01 +0000

    colocado tipos,id, nome  imagens aos pokemons

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -5,8 +5,31 @@ const Pokemon = (props) => {
     console.log("pokemons ", pokemon);
         
     return (
-        <div>
-            {pokemon.name}
+        <div className='pokemon-card'>
+            <div className='pokemon-image-container' > 
+                <img alt={pokemon.name} src={pokemon.sprites.front_default} className='pokemon-image'  />
+            </div>
+            <div className='card-body' ></div>
+            <div className='card-top' >
+                <h3> {pokemon.name}</h3> 
+                <div> #{pokemon.id} </div>
+            </div>
+            <div className='card-bottom' >
+               <div className='pokemon-type'>
+                    <div>{pokemon.types.map((type, index) => {
+                        return (<div key={index} className='pokemon-type-text'>
+                            {type.type.name}
+                             </div>)          
+                    })}
+                    </div>
+                    <button className='pokemon-heart-btn' onclick={onHeartClick}>
+                        {heart}
+                    </button>
+
+               </div>
+            </div>
+           
+
         </div>
     )
 }
